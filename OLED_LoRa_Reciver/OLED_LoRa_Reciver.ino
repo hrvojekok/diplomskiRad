eca740f8a78a33fd15584b40b5732504b365288b
Hrvoje Kokosarevic <hrvoje.kok@gmail.com>
2019-05-09 22:42:39 +0200

    added server

---
+++ OLED_LoRa_Reciver/OLED_LoRa_Reciver.ino
@@ -0,0 +1,157 @@
+#include <SPI.h>
+#include <LoRa.h>
+#include <Wire.h>  
+#include "SSD1306.h" 
+#include <WiFi.h>
+//#include "images.h"
+
+#define SCK     5    // GPIO5  -- SX1278's SCK
+#define MISO    19   // GPIO19 -- SX1278's MISO
+#define MOSI    27   // GPIO27 -- SX1278's MOSI
+#define SS      18   // GPIO18 -- SX1278's CS
+#define RST     14   // GPIO14 -- SX1278's RESET
+#define DI0     26   // GPIO26 -- SX1278's IRQ(Interrupt Request)
+#define BAND    433E6
+
+//wifi pass and ssid
+const char* ssid     = "6486EF";
+const char* password = "EVW32C0S00021595";
+
+WiFiServer server(80);
+
+
+SSD1306 display(0x3c, 4, 15);
+String rssi = "RSSI --";
+String packSize = "--";
+String packet ;
+
+
+
+void loraData(){
+  display.clear();
+  display.setTextAlignment(TEXT_ALIGN_LEFT);
+  display.setFont(ArialMT_Plain_10);
+  display.drawString(0 , 15 , "Received "+ packSize + " bytes");
+  display.drawStringMaxWidth(0 , 26 , 128, packet);
+  display.drawString(0, 0, rssi);  
+  display.display();
+}
+
+void cbk(int packetSize) {
+  packet ="";
+  packSize = String(packetSize,DEC);
+  for (int i = 0; i < packetSize; i++) { packet += (char) LoRa.read(); }
+  rssi = "RSSI " + String(LoRa.packetRssi(), DEC) ;
+  loraData();
+}
+
+
+String cbkString(int packetSize) {
+  String data;
+  packet ="";
+  packSize = String(packetSize,DEC);
+  for (int i = 0; i < packetSize; i++) { packet += (char) LoRa.read(); }
+  rssi = "RSSI " + String(LoRa.packetRssi(), DEC) ;
+  loraData();
+}
+
+void setup() {
+  Serial.begin(9600);
+  
+  pinMode(16,OUTPUT);
+  digitalWrite(16, LOW);    // set GPIO16 low to reset OLED
+  delay(50); 
+  digitalWrite(16, HIGH); // while OLED is running, must set GPIO16 in high、
+
+  //set up wifi
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+  server.begin();
+  //set up wifi - end
+  
+  while (!Serial);
+  Serial.println();
+  Serial.println("LoRa Receiver Callback");
+  SPI.begin(SCK,MISO,MOSI,SS);
+  LoRa.setPins(SS,RST,DI0);  
+  if (!LoRa.begin(433E6)) {
+    Serial.println("Starting LoRa failed!");
+    while (1);
+  }
+  //LoRa.onReceive(cbk);
+  LoRa.receive();
+  Serial.println("init ok");
+  display.init();
+  display.flipScreenVertically();  
+  display.setFont(ArialMT_Plain_10);
+  
+  delay(1500);
+}
+
+void loop() {
+  int packetSize = LoRa.parsePacket();
+  //String data;
+  if (packetSize) { 
+    //data = cbkString(packetSize);
+    cbk(packetSize);
+    }
+  delay(10);
+
+  WiFiClient client = server.available();   // listen for incoming clients
+
+  if (client) {                             // if you get a client,
+    Serial.println("new client");           // print a message out the serial port
+    String currentLine = "";                // make a String to hold incoming data from the client
+    while (client.connected()) {            // loop while the client's connected
+      if (client.available()) {             // if there's bytes to read from the client,
+        char c = client.read();             // read a byte, then
+        Serial.write(c);                    // print it out the serial monitor
+        if (c == '\n') {                    // if the byte is a newline character
+
+          // if the current line is blank, you got two newline characters in a row.
+          // that's the end of the client HTTP request, so send a response:
+          if (currentLine.length() == 0) {
+            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
+            // and a content-type so the client knows what's coming, then a blank line:
+            client.println("HTTP/1.1 200 OK");
+            client.println("Content-type:text/html");
+            client.println();
+
+            // the content of the HTTP response follows the header:
+            client.print("Click <a href=\"/H\">here</a> turn the LED on pin 5 on<br>");
+            client.print("Click <a href=\"/L\">here</a> turn the LED on pin 5 off<br>");
+            //client.print(data);
+
+            // The HTTP response ends with another blank line:
+            client.println();
+            // break out of the while loop:
+            break;
+          } else {    // if you got a newline, then clear currentLine:
+            currentLine = "";
+          }
+        } else if (c != '\r') {  // if you got anything else but a carriage return character,
+          currentLine += c;      // add it to the end of the currentLine
+        }
+
+        // Check to see if the client request was "GET /H" or "GET /L":
+        if (currentLine.endsWith("GET /H")) {
+          digitalWrite(5, HIGH);               // GET /H turns the LED on
+        }
+        if (currentLine.endsWith("GET /L")) {
+          digitalWrite(5, LOW);                // GET /L turns the LED off
+        }
+      }
+    }
+    // close the connection:
+    client.stop();
+    Serial.println("client disonnected");
+  }
+}
