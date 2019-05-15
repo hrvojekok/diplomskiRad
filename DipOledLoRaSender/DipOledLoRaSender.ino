aab3c7ef8065f1325cf81f61d7e4f61e7ccf11ac
Hrvoje Kokosarevic <hrvoje.kok@gmail.com>
2019-05-15 21:26:08 +0200

    added functionality for all 4 sensors

--- DipOledLoRaSender/DipOledLoRaSender.ino
+++ DipOledLoRaSender/DipOledLoRaSender.ino
@@ -13,8 +13,14 @@
 #define DI0     26
 #define BAND    433E6  //915E6 - frequency for LoRa 433MHz/915MHz
 
+int first = 36;
+int second = 39;
+int third = 34;
+
 int counter = 0;
-int value = 0;
+int value1 = 0;
+int value2 = 0;
+int value3 = 0;
 
 //SSD1306  display(0x3c, 4, 15);
 
@@ -30,6 +36,11 @@ void setup() {
 //  delay(50); 
 //  digitalWrite(16, HIGH); // while OLED is running, must set GPIO16 in high
 
+  pinMode(first, INPUT);
+  pinMode(second, INPUT);
+  pinMode(third, INPUT);
+
+  
   Serial.begin(115200);
 
   //added
@@ -75,7 +86,9 @@ void loop() {
   Serial.println();
   //added end
   
-  value = analogRead(A0);
+  value1 = analogRead(first);
+  value2 = analogRead(second);
+  value3 = analogRead(third);
   //Serial.println(value);
   //Serial.println(value);
 
@@ -85,8 +98,13 @@ void loop() {
   //int percentageValue = convertToPercentages(value);
   
   //when using capacitive soil moisture sensor use convertToPercentagesCapacitive function
-  int percentageValue = convertToPercentagesCapacitive(value);
-  Serial.println(percentageValue);
+  int percentageValue1 = convertToPercentagesCapacitive(value1);
+  int percentageValue2 = convertToPercentages(value2);
+  int percentageValue3 = convertToPercentages(value3);
+  
+  Serial.println(percentageValue1);
+  Serial.println(percentageValue2);
+  Serial.println(percentageValue3);
 
 //  display.drawString(0, 10, "Soil moisture: ");
 //  display.drawString(65, 10, String(percentageValue));
@@ -99,19 +117,32 @@ void loop() {
 
   // send packet
   LoRa.beginPacket();
-  LoRa.print(value);
-  LoRa.print(" ");
+  LoRa.print(value1);
+  LoRa.print(" | ");
+  LoRa.print(percentageValue1);
+  LoRa.print(" | ");
+  LoRa.print(value2);
+  LoRa.print(" | ");
+  LoRa.print(percentageValue2);
+  LoRa.print(" | ");
+  LoRa.print(value3);
+  LoRa.print(" | ");
+  LoRa.print(percentageValue3);
+  LoRa.print(" | ");
   LoRa.print(temp);
-  LoRa.print(" ");
+  LoRa.print(" | ");
   LoRa.print(humd);
-  LoRa.print(" Soil moisture: ");
-  LoRa.print(percentageValue);
-  LoRa.print("%  ");
+  LoRa.print(" | counter: ");
+//  LoRa.print(" Soil moisture: ");
+//  LoRa.print(percentageValue1);
+//  LoRa.print("%  ");
   LoRa.print(counter);
   LoRa.endPacket();
   delay(300);
   counter++;
-  value = 0;
+  value1 = 0;
+  value2 = 0;
+  value3 = 0;
 
 }
 
